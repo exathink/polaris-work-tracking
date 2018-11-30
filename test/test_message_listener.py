5517c8217fce47cd9d26f1702ee5f369c32d4a2d
Krishna Kumar <kkumar@exathink.com>
2018-11-30 13:24:38 -0600

    Updated message_listener to use new TopicSubscriber base class and dispatch mechanism [ #162224792 (https://www.pivotaltracker.com/story/show/162224792)]

--- test/test_message_listener.py
+++ test/test_message_listener.py
@@ -10,18 +10,19 @@
 
 from collections import namedtuple
 from datetime import datetime
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 import pytest
-from polaris.messaging.utils import unpack_message
+from pika.channel import Channel
+
 
 from polaris.messaging.messages import CommitsCreated, CommitWorkItemsResolved, WorkItemsCommitsResolved, WorkItemsCommitsUpdated
-from polaris.work_tracking import message_listener
+from polaris.work_tracking.message_listener import CommitsTopicSubscriber, WorkItemsTopicSubscriber
 from polaris.common import db
 from test.constants import *
 from .helpers import find_work_items
 
-method_shim = namedtuple('method_shim', 'routing_key')
+mock_channel = MagicMock(Channel)
 
 # ----------------------------------------
 # TestCommitsCreated
@@ -57,7 +58,7 @@ def commit_created_message(setup_work_items):
         ]
     )
 
-    yield CommitsCreated(send=payload).message_body
+    yield CommitsCreated(send=payload)
 
 @pytest.yield_fixture()
 def commit_created_no_work_items_to_resolve_message(setup_work_items):
@@ -89,51 +90,42 @@ def commit_created_no_work_items_to_resolve_message(setup_work_items):
         ]
     )
 
-    yield CommitsCreated(send=payload).message_body
+    yield CommitsCreated(send=payload)
 
-class TestCommitsCreatedMessage:
-    def it_returns_a_valid_response(self, commit_created_message):
-        with patch('polaris.messaging.topics.Topic.publish'):
-            response_messages = message_listener.commits_topic_dispatch(None, method_shim(routing_key=CommitsCreated.message_type), None, commit_created_message)
-            assert len(response_messages) == 2
-            assert response_messages[0].message_type == CommitWorkItemsResolved.message_type
-            assert CommitWorkItemsResolved(receive=response_messages[0].message_body).dict
-            assert response_messages[1].message_type == WorkItemsCommitsResolved.message_type
-            assert WorkItemsCommitsResolved(receive=response_messages[1].message_body).dict
+class TestCommitsTopicSubscriber:
 
-    def it_publishes_the_response_correctly(self, commit_created_message):
-        with patch('polaris.messaging.topics.CommitsTopic.publish') as commits_topic_publish:
-            with patch('polaris.messaging.topics.WorkItemsTopic.publish') as work_items_topic_publish:
-                response = message_listener.commits_topic_dispatch(None, method_shim(routing_key=CommitsCreated.message_type), None, commit_created_message)
-                commits_topic_publish.assert_called_with(
-                    message=response[0]
-                )
-                work_items_topic_publish.assert_called_with(
-                    message=response[1]
-                )
+    class TestCommitsCreatedMessage:
+        def it_returns_a_valid_response(self, commit_created_message):
+            with patch('polaris.messaging.topics.Topic.publish'):
+                response_messages = CommitsTopicSubscriber(mock_channel).dispatch(mock_channel, commit_created_message)
+                assert len(response_messages) == 2
+                assert response_messages[0].message_type == CommitWorkItemsResolved.message_type
+                assert CommitWorkItemsResolved(receive=response_messages[0].message_body).dict
+                assert response_messages[1].message_type == WorkItemsCommitsResolved.message_type
+                assert WorkItemsCommitsResolved(receive=response_messages[1].message_body).dict
 
-    def it_publishes_a_response_even_if_no_work_items_were_resolved(self, commit_created_no_work_items_to_resolve_message):
-        with patch('polaris.messaging.topics.CommitsTopic.publish') as commits_topic_publish:
-            with patch('polaris.messaging.topics.WorkItemsTopic.publish') as work_items_topic_publish:
-                response = message_listener.commits_topic_dispatch(None, method_shim(routing_key=CommitsCreated.message_type), None, commit_created_no_work_items_to_resolve_message)
-                commits_topic_publish.assert_called_with(
-                    message=response[0]
-                )
-                work_items_topic_publish.assert_called_with(
-                    message=response[1]
-                )
+        def it_publishes_the_response_correctly(self, commit_created_message):
+            with patch('polaris.messaging.topics.CommitsTopic.publish') as commits_topic_publish:
+                with patch('polaris.messaging.topics.WorkItemsTopic.publish') as work_items_topic_publish:
+                    subscriber = CommitsTopicSubscriber(mock_channel)
+                    response = subscriber.dispatch(mock_channel, commit_created_message)
+                    commits_topic_publish.assert_called_with(
+                        message=response[0]
+                    )
+                    work_items_topic_publish.assert_called_with(
+                        message=response[1]
+                    )
 
-
-    def it_publishes_processing_errors(self, commit_created_message):
-        with patch('polaris.messaging.topics.PolarisErrorsTopic.publish') as error_topic_publish:
-            with patch('polaris.work_tracking.message_listener.process_commits_created') as processing:
-                processing.side_effect = Exception('oops')
-                message_listener.commits_topic_dispatch(None, method_shim(routing_key=CommitsCreated.message_type), None, commit_created_message)
-
-                error_topic_publish.assert_called()
-                processing_error_message = error_topic_publish.call_args[0][0]
-                assert processing_error_message.dict.get('error') == 'oops'
-                assert processing_error_message.dict.get('original_message') == unpack_message(commit_created_message)
+        def it_publishes_a_response_even_if_no_work_items_were_resolved(self, commit_created_no_work_items_to_resolve_message):
+            with patch('polaris.messaging.topics.CommitsTopic.publish') as commits_topic_publish:
+                with patch('polaris.messaging.topics.WorkItemsTopic.publish') as work_items_topic_publish:
+                    response = CommitsTopicSubscriber(mock_channel).dispatch(mock_channel, commit_created_no_work_items_to_resolve_message)
+                    commits_topic_publish.assert_called_with(
+                        message=response[0]
+                    )
+                    work_items_topic_publish.assert_called_with(
+                        message=response[1]
+                    )
 
 # ----------------------------------------
 # Test WorkItemCommitsResolved
@@ -170,7 +162,7 @@ def work_items_commits_resolved_message(setup_work_items):
         ]
     )
 
-    yield WorkItemsCommitsResolved(send=payload).message_body
+    yield WorkItemsCommitsResolved(send=payload)
 
     db.connection().execute("delete from work_tracking.work_items_commits")
     db.connection().execute("delete from work_tracking.cached_commits")
@@ -181,7 +173,7 @@ class TestWorkItemsCommitsResolved:
 
     def it_returns_a_valid_response(self, work_items_commits_resolved_message):
         with patch('polaris.messaging.topics.Topic.publish'):
-            response_message = message_listener.work_items_topic_dispatch(None, method_shim(routing_key=WorkItemsCommitsResolved.message_type), None, work_items_commits_resolved_message)
+            response_message = WorkItemsTopicSubscriber(mock_channel).dispatch(mock_channel, work_items_commits_resolved_message)
             assert response_message
             assert response_message.message_type == WorkItemsCommitsUpdated.message_type
             assert WorkItemsCommitsUpdated(receive=response_message.message_body).dict
@@ -189,7 +181,7 @@ class TestWorkItemsCommitsResolved:
 
     def it_publishes_the_response_correctly(self, work_items_commits_resolved_message):
         with patch('polaris.messaging.topics.WorkItemsTopic.publish') as work_item_topic_publish:
-            response_message = message_listener.work_items_topic_dispatch(None, method_shim(routing_key=WorkItemsCommitsResolved.message_type), None, work_items_commits_resolved_message)
+            response_message = WorkItemsTopicSubscriber(mock_channel).dispatch(mock_channel, work_items_commits_resolved_message)
             work_item_topic_publish.assert_called_with(message=response_message)
 
     def it_processes_and_publishes_the_response_even_if_there_are_no_commits_to_update(self):
@@ -200,21 +192,11 @@ class TestWorkItemsCommitsResolved:
                     repository_name='rails',
                     work_items_commits=[]
                 )
-            ).message_body
-            response_message = message_listener.work_items_topic_dispatch(None, method_shim(routing_key=WorkItemsCommitsResolved.message_type), None, no_work_items_message )
+            )
+            response_message = WorkItemsTopicSubscriber(mock_channel).dispatch(mock_channel, no_work_items_message )
             work_item_topic_publish.assert_called_with(message=response_message)
 
-    def it_publishes_processing_errors(self, work_items_commits_resolved_message):
-        with patch('polaris.messaging.topics.PolarisErrorsTopic.publish') as error_topic_publish:
-            with patch('polaris.work_tracking.message_listener.process_work_items_commits_resolved') as processing:
-                processing.side_effect = Exception('oops')
 
-                response_message = message_listener.work_items_topic_dispatch(None, method_shim(routing_key=WorkItemsCommitsResolved.message_type), None, work_items_commits_resolved_message)
-
-                error_topic_publish.assert_called()
-                processing_error_message = error_topic_publish.call_args[0][0]
-                assert processing_error_message.dict.get('error') == 'oops'
-                assert processing_error_message.dict.get('original_message') == unpack_message(work_items_commits_resolved_message)
 
 
 
