cf3fadf729fe202ec89927064720925a3bcce79e
Krishna Kumar <kkumar@exathink.com>
2019-01-21 13:23:06 -0600

    Make cli publish a message instead of hitting the db api. [story=#163364160 subject=migrate-on-work-item-created story_url=https://www.pivotaltracker.com/story/show/163364160]

--- polaris/work_tracking/cli.py
+++ polaris/work_tracking/cli.py
@@ -12,27 +12,26 @@ import logging
 import argh
 from polaris.utils.token_provider import get_token_provider
 from polaris.utils.logging import config_logging
-from polaris.work_tracking.work_tracker import sync_work_items as api_import_work_items
 
-from polaris.common import db
+from polaris.messaging.utils import publish
+from polaris.messaging.topics import WorkItemsTopic
+from polaris.messaging.messages import ImportWorkItems
+
+
 
 logger = logging.getLogger('polaris.work_tracking.cli')
 token_provider = get_token_provider()
 
+
 def import_work_items(work_items_source_key):
-    result = api_import_work_items(token_provider, work_items_source_key)
-    if result['total'] > 0:
-        logger.log(logging.INFO, f"{len(result['created'])} new work items created")
-        logger.log(logging.INFO, f"{result['updated']} work items updated")
-    else:
-        logger.log(logging.INFO, f"No updates found for work items source {work_items_source_key}")
+    publish(WorkItemsTopic, ImportWorkItems(send=dict(work_items_source_key=work_items_source_key)))
 
 
 if __name__ == '__main__':
     config_logging(
         suppress=['requests.packages.urllib3.connectionpool']
     )
-    db.init()
+
 
     argh.dispatch_commands([
         import_work_items
