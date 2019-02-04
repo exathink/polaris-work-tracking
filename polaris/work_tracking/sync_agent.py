45132ff72f00d4eafa189f49331a276377080224
Krishna Kumar <kkumar@exathink.com>
2019-02-04 07:51:23 -0600

    improve logging and shutdown handling in sync-agent.
    [story=#163627999 subject=add-work-items-source story_url=https://www.pivotaltracker.com/story/show/163627999]

--- polaris/work_tracking/sync_agent.py
+++ polaris/work_tracking/sync_agent.py
@@ -15,8 +15,11 @@ from polaris.utils.logging import config_logging
 from polaris.common import db
 from polaris.work_tracking.db import api
 from polaris.messaging.topics import WorkItemsTopic
-from polaris.messaging.utils import publish
+from polaris.messaging.utils import publish, init_topics_to_publish, shutdown
 from polaris.messaging.messages import ImportWorkItems
+from logging import getLogger
+
+logger = getLogger('polaris.work_tracking.sync_agent')
 
 
 class WorkTrackingAgent(Agent):
@@ -24,15 +27,23 @@ class WorkTrackingAgent(Agent):
     def run(self):
         self.loop(self.sync_work_item_sources)
 
-    @staticmethod
-    def sync_work_item_sources():
+    def sync_work_item_sources(self):
+        logger.info("Checking for work items sources to sync")
+        found = False
         for source in api.get_work_items_sources_to_sync():
+            found = True
             publish(
                 WorkItemsTopic,
                 ImportWorkItems(send=source)
             )
+            if self.exit_signal_received:
+                shutdown()
+                break
 
+        if not found:
+            logger.info("No sources found to sync...")
         return True
+
 
 
 def start(name=None, poll_interval=None):
@@ -40,13 +51,17 @@ def start(name=None, poll_interval=None):
         name=name,
         poll_interval=poll_interval
     )
+    logger.info("Starting agent.")
     agent.run()
 
 
 if __name__ == '__main__':
 
     config_logging()
+    logger.info("Connecting to database....")
     db.init()
+    logger.info("Initializing messaging..")
+    init_topics_to_publish(WorkItemsTopic)
 
     argh.dispatch_commands([
         start
