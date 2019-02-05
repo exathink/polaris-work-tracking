decabfcccd7152bef0f188d1e40240e9a89878b1
Krishna Kumar <kkumar@exathink.com>
2019-02-04 20:10:29 -0600

    Complete deployment script for work-tracking

--- cli/deploy.sh
+++ cli/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export DONT_PROMPT_FOR_CONFIRMATION=1
 echo "Deploying Image"
 package aws deploy-image
 
@@ -6,5 +7,9 @@ echo "Deploying Task Definitions.."
 package aws deploy-task-definition polaris-work-tracking-db-migrator
 package aws deploy-task-definition polaris-work-tracking-service
 package aws deploy-task-definition polaris-work-tracking-listener
-package aws deploy-task-definition polaris-work-tracking-sync-agent
 
+echo "Running migrations"
+package aws run-task polaris-work-tracking-db-migrator upgrade head
+
+echo "Deploying Services.."
+package aws deploy-services polaris-work-tracking-service polaris.auto-scaling-group polaris-work-tracking-service polaris-work-tracking-listener
