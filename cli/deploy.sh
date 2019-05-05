011f581eadfd28c8c20cce14fe65096a608f25ee
Krishna Kumar <kkumar@exathink.com>
2019-05-05 14:57:02 -0500

    updates to fix misc deployment issues [ #165811339 (https://www.pivotaltracker.com/story/show/165811339)]

--- cli/deploy.sh
+++ cli/deploy.sh
@@ -10,7 +10,7 @@ package aws deploy-task-definition polaris-work-tracking-service
 package aws deploy-task-definition polaris-work-tracking-listener
 
 echo "Running migrations"
-# package aws run-task polaris-work-tracking-db-migrator
+package aws run-task polaris-work-tracking-db-migrator
 
 echo "Deploying Services.."
 package aws deploy-services polaris-work-tracking-service polaris.auto-scaling-group polaris-work-tracking-service polaris-work-tracking-listener
