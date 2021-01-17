26ca0fa2d0d5d6ec2891087540447ff64731ad5c
Pragya Goyal <pragya@64sqs.com>
2021-01-17 16:37:11 +0530

    Added test for mutation for a new webhook registration for work items source

---
+++ test/graphql/mutations/test_register_work_items_source_connector_webhooks.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: © Exathink, LLC (2011-2018) All Rights Reserved
+
+# Unauthorized use or copying of this file and its contents, via any medium
+# is strictly prohibited. The work product in this file is proprietary and
+# confidential.
+
+# Author: Pragya Goyal
+
+import uuid
+import pytest
+from unittest.mock import patch
+from test.constants import *
+from polaris.utils.collections import Fixture
+from graphene.test import Client
+from polaris.work_tracking.service.graphql import schema
+from polaris.common import db
+
+
+class TestRegisterWorkItemsSourceConnectorWebhooks:
+    class TestWithGitlabConnector:
+
+        @pytest.yield_fixture()
+        def setup(self, setup_work_item_sources):
+            session, work_items_sources = setup_work_item_sources
+            session.commit()
+            gitlab_work_items_source_key = work_items_sources['gitlab'].key
+            connector_key = work_items_sources['gitlab'].connector_key
+            active_hook_id = '1000'
+            registered_events = ['issues_events']
+            mutation_string = """
+                mutation registerWorkItemsSourceConnectorWebhooks($registerWebhooksInput: RegisterWebhooksInput!) {
+                    registerWorkItemsSourceConnectorWebhooks(registerWebhooksInput: $registerWebhooksInput){
+                    webhooksRegistrationStatus {
+                      workItemsSourceKey
+                      success
+                      message
+                    }
+                  }
+                }
+            """
+            yield Fixture(
+                organization_key=polaris_organization_key,
+                work_items_source_key=gitlab_work_items_source_key,
+                connector_key=connector_key,
+                mutation_string=mutation_string,
+                registered_events=registered_events,
+                active_hook_id=active_hook_id
+            )
+
+        def it_registers_new_webhook_and_updates_info(self, setup):
+            fixture = setup
+            client = Client(schema)
+
+            with patch(
+                    'polaris.work_tracking.integrations.gitlab.GitlabWorkTrackingConnector.register_project_webhooks'
+            ) as register_webhooks:
+                register_webhooks.return_value = dict(
+                    success=True,
+                    active_webhook=fixture.active_hook_id,
+                    deleted_webhooks=[],
+                    registered_events=fixture.registered_events
+                )
+                response = client.execute(
+                    fixture.mutation_string,
+                    variable_values=dict(
+                        registerWebhooksInput=dict(
+                            connectorKey=str(fixture.connector_key),
+                            workItemsSourceKeys=[str(fixture.work_items_source_key)]
+                        )
+                    )
+                )
+                assert 'data' in response
+                status = response['data']['registerWorkItemsSourceConnectorWebhooks']['webhooksRegistrationStatus']
+                assert len(status) == 1
+                assert status[0]['success']
+
+                assert db.connection().execute(
+                    f"select count(*) from work_tracking.work_items_sources \
+                    where key='{fixture.work_items_source_key}' \
+                    and source_data->>'active_webhook'='{fixture.active_hook_id}'" \
+                    ).scalar() == 1
