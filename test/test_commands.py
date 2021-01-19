871096a5d2d0f483120d5b3eaeae22a1950f4643
Pragya Goyal <pragya@64sqs.com>
2021-01-19 19:55:52 +0530

    Fixed failing tests by mocking result for fetch_project_boards api

--- test/test_commands.py
+++ test/test_commands.py
@@ -56,26 +56,137 @@ class TestGitlabSyncWorkItems:
         empty_source = work_items_sources['gitlab']
         with patch(
                 'polaris.work_tracking.integrations.gitlab.GitlabProject.fetch_work_items_to_sync') as fetch_work_items_to_sync:
-            fetch_work_items_to_sync.return_value = [new_work_items]
+            with patch('polaris.work_tracking.integrations.gitlab.GitlabProject.fetch_project_boards') as fetch_project_boards:
+                fetch_work_items_to_sync.return_value = [new_work_items]
+                fetch_project_boards.return_value = [
+                    {
+                        "id": 2245441,
+                        "name": "Development",
+                        "hide_backlog_list": False,
+                        "hide_closed_list": False,
+                        "lists": [
+                            {
+                                "id": 6786201,
+                                "label": {
+                                    "id": 17640856,
+                                    "name": "IN-PROGRESS",
+                                    "color": "#5CB85C",
+                                    "description": "Indicates in progress issues",
+                                    "description_html": "Indicates in progress issues",
+                                    "text_color": "#FFFFFF"
+                                },
+                                "position": 0
+                            },
+                            {
+                                "id": 6786202,
+                                "label": {
+                                    "id": 17640857,
+                                    "name": "DEV-DONE",
+                                    "color": "#A295D6",
+                                    "description": None,
+                                    "description_html": "",
+                                    "text_color": "#333333"
+                                },
+                                "position": 1
+                            }
+                        ]
+                    },
+                    {
+                        "id": 2282923,
+                        "name": "Product",
+                        "hide_backlog_list": False,
+                        "hide_closed_list": False,
+                        "lists": [
+                            {
+                                "id": 6786204,
+                                "label": {
+                                    "id": 17640863,
+                                    "name": "DEPLOYED",
+                                    "color": "#428BCA",
+                                    "description": "Story has been deployed",
+                                    "description_html": "Story has been deployed",
+                                    "text_color": "#FFFFFF"
+                                },
+                                "position": 0
+                            }
+                        ],
+                    }
+                ]
 
-            for result in commands.sync_work_items(token_provider, empty_source.key):
-                assert len(result) == len(new_work_items)
-                assert all(map(lambda item: item['is_new'], result))
+                for result in commands.sync_work_items(token_provider, empty_source.key):
+                    assert len(result) == len(new_work_items)
+                    assert all(map(lambda item: item['is_new'], result))
 
     def it_updates_work_items_that_already_exist(self, setup_work_items, new_work_items):
         _, work_items_sources = setup_work_items
         empty_source = work_items_sources['gitlab']
         with patch(
                 'polaris.work_tracking.integrations.gitlab.GitlabProject.fetch_work_items_to_sync') as fetch_work_items_to_sync:
-            fetch_work_items_to_sync.return_value = [new_work_items]
-            # import once and ignore results
-            for _ in commands.sync_work_items(token_provider, empty_source.key):
-                pass
+            with patch('polaris.work_tracking.integrations.gitlab.GitlabProject.fetch_project_boards') as fetch_project_boards:
+                fetch_work_items_to_sync.return_value = [new_work_items]
+                fetch_project_boards.return_value = [
+                    {
+                        "id": 2245441,
+                        "name": "Development",
+                        "hide_backlog_list": False,
+                        "hide_closed_list": False,
+                        "lists": [
+                            {
+                                "id": 6786201,
+                                "label": {
+                                    "id": 17640856,
+                                    "name": "IN-PROGRESS",
+                                    "color": "#5CB85C",
+                                    "description": "Indicates in progress issues",
+                                    "description_html": "Indicates in progress issues",
+                                    "text_color": "#FFFFFF"
+                                },
+                                "position": 0
+                            },
+                            {
+                                "id": 6786202,
+                                "label": {
+                                    "id": 17640857,
+                                    "name": "DEV-DONE",
+                                    "color": "#A295D6",
+                                    "description": None,
+                                    "description_html": "",
+                                    "text_color": "#333333"
+                                },
+                                "position": 1
+                            }
+                        ]
+                    },
+                    {
+                        "id": 2282923,
+                        "name": "Product",
+                        "hide_backlog_list": False,
+                        "hide_closed_list": False,
+                        "lists": [
+                            {
+                                "id": 6786204,
+                                "label": {
+                                    "id": 17640863,
+                                    "name": "DEPLOYED",
+                                    "color": "#428BCA",
+                                    "description": "Story has been deployed",
+                                    "description_html": "Story has been deployed",
+                                    "text_color": "#FFFFFF"
+                                },
+                                "position": 0
+                            }
+                        ],
+                    }
+                ]
+                fetch_work_items_to_sync.return_value = [new_work_items]
+                # import once and ignore results
+                for _ in commands.sync_work_items(token_provider, empty_source.key):
+                    pass
 
-            # import again
-            for result in commands.sync_work_items(token_provider, empty_source.key):
-                assert len(result) == len(new_work_items)
-                assert all(map(lambda item: not item['is_new'], result))
+                # import again
+                for result in commands.sync_work_items(token_provider, empty_source.key):
+                    assert len(result) == len(new_work_items)
+                    assert all(map(lambda item: not item['is_new'], result))
 
 class TestJiraSyncWorkItemsForEpic:
 
