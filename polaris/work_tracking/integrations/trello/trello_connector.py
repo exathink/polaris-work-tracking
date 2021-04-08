bfe2a4d643be8c0e5084561f03ee36a453388bab
Pragya Goyal <pragya@64sqs.com>
2021-04-08 18:03:50 +0530

    Trello fetch boards code added and tested

--- polaris/work_tracking/integrations/trello/trello_connector.py
+++ polaris/work_tracking/integrations/trello/trello_connector.py
@@ -10,11 +10,12 @@
 
 import logging
 import requests
+from enum import Enum
 
 from polaris.integrations.trello import TrelloConnector
 from polaris.utils.config import get_config_provider
 from polaris.utils.exceptions import ProcessingException
-
+from polaris.common.enums import WorkTrackingIntegrationType
 
 config_provider = get_config_provider()
 
@@ -26,12 +27,26 @@ class TrelloWorkTrackingConnector(TrelloConnector):
     def __init__(self, connector):
         super().__init__(connector)
 
+    def map_project_to_work_items_sources_data(self, project):
+        return dict(
+            integration_type=WorkTrackingIntegrationType.trello.value,
+            work_items_source_type=TrelloWorkItemSourceType.projects.value,
+            parameters=dict(),
+            commit_mapping_scope='organization',
+            source_id=project['id'],
+            name=project['name'],
+            url=project['url'],
+            description=project['desc'],
+            custom_fields=[]
+        )
+
     def fetch_trello_boards(self):
         fetch_boards_url = f'{self.base_url}/members/me/boards'
         while fetch_boards_url is not None:
             response = requests.get(
                 fetch_boards_url,
-                headers={'Authorization': f'OAuth oauth_consumer_key="{self.api_key}", oauth_token="{self.access_token}"'}
+                headers={
+                    'Authorization': f'OAuth oauth_consumer_key="{self.api_key}", oauth_token="{self.access_token}"'}
             )
             if response.ok:
                 yield response.json()
@@ -45,4 +60,12 @@ class TrelloWorkTrackingConnector(TrelloConnector):
                 )
 
     def fetch_work_items_sources_to_sync(self):
-        pass
+        for boards in self.fetch_trello_boards():
+            yield [
+                self.map_project_to_work_items_sources_data(board)
+                for board in boards
+            ]
+
+
+class TrelloWorkItemSourceType(Enum):
+    projects = 'boards'
