81fa788ed9ed14f81776039a7e2292ccfc923a38
Pragya Goyal <pragya@64sqs.com>
2021-04-08 16:25:08 +0530

    Added fetch_trello_boards api call

--- polaris/work_tracking/integrations/trello/trello_connector.py
+++ polaris/work_tracking/integrations/trello/trello_connector.py
@@ -9,9 +9,12 @@
 # Author: Pragya Goyal
 
 import logging
+import requests
 
 from polaris.integrations.trello import TrelloConnector
 from polaris.utils.config import get_config_provider
+from polaris.utils.exceptions import ProcessingException
+
 
 config_provider = get_config_provider()
 
@@ -22,3 +25,24 @@ class TrelloWorkTrackingConnector(TrelloConnector):
 
     def __init__(self, connector):
         super().__init__(connector)
+
+    def fetch_trello_boards(self):
+        fetch_boards_url = f'{self.base_url}/members/me/boards'
+        while fetch_boards_url is not None:
+            response = requests.get(
+                fetch_boards_url,
+                headers={'Authorization': f'OAuth oauth_consumer_key="{self.api_key}", oauth_token="{self.access_token}"'}
+            )
+            if response.ok:
+                yield response.json()
+                if 'next' in response.links:
+                    fetch_boards_url = response.links['next']['url']
+                else:
+                    fetch_boards_url = None
+            else:
+                raise ProcessingException(
+                    f"Trello Boards fetch failed {response.text} status: {response.status_code}\n"
+                )
+
+    def fetch_work_items_sources_to_sync(self):
+        pass
