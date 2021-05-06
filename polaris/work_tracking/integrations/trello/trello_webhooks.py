df434047a105567458f06847a04819f864b7031a
Pragya Goyal <pragya@64sqs.com>
2021-05-06 17:43:11 +0530

    Added publish step in webhook callback function

--- polaris/work_tracking/integrations/trello/trello_webhooks.py
+++ polaris/work_tracking/integrations/trello/trello_webhooks.py
@@ -20,7 +20,7 @@ webhook = Blueprint('trello_webhooks', __name__)
 @webhook.route(f"/project/webhooks/<connector_key>/", methods=('GET', 'POST'))
 def project_webhooks(connector_key):
     logger.info('Received webhook event @project/webhooks')
-
-    #event_type = request.json['object_kind']
-    #publish.gitlab_project_event(event_type, connector_key, request.data)
+    action = request.json['action']
+    event_type = action['type']
+    publish.trello_board_event(event_type, connector_key, request.data)
     return ''
