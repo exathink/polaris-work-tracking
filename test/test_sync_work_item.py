486161b8e04f263f90bba28a4454279a933f7456
Pragya Goyal <pragya@64sqs.com>
2021-06-07 17:34:28 +0530

    Test for case when connector does not define sync_work_item (all non jira connectors)

--- test/test_sync_work_item.py
+++ test/test_sync_work_item.py
@@ -17,7 +17,7 @@ from .fixtures.jira_fixtures import *
 
 token_provider = get_token_provider()
 
-work_items_common = dict(
+work_items_common_jira = dict(
     work_item_type=JiraWorkItemType.epic.value,
     description='Foo',
     is_bug=False,
@@ -30,24 +30,24 @@ work_items_common = dict(
 
 
 @pytest.fixture
-def new_work_items():
+def new_work_items_jira():
     return [
         dict(
             name=f'Issue {i}',
             source_id=str(i),
             source_display_id=str(i),
             url=f'http://foo.com/{i}',
-            **work_items_common
+            **work_items_common_jira
         )
         for i in range(100, 110)
     ]
 
 
-class TestSyncWorkItemForJira:
+class TestSyncWorkItem:
 
     def it_assigns_work_item_key_to_new_work_item(self, jira_work_item_source_fixture, cleanup):
         work_items_source, jira_project_id, connector_key = jira_work_item_source_fixture
-        new_work_item = new_work_items()[0]
+        new_work_item = new_work_items_jira()[0]
         with patch(
                 'polaris.work_tracking.integrations.atlassian.jira_work_items_source.JiraProject.fetch_work_item') as fetch_work_item:
             fetch_work_item.return_value = [new_work_item]
@@ -60,7 +60,7 @@ class TestSyncWorkItemForJira:
 
     def it_updates_work_item_that_already_exists(self, jira_work_item_source_fixture, cleanup):
         work_items_source, jira_project_id, connector_key = jira_work_item_source_fixture
-        new_work_item = new_work_items()[0]
+        new_work_item = new_work_items_jira()[0]
         with patch(
                 'polaris.work_tracking.integrations.atlassian.jira_work_items_source.JiraProject.fetch_work_item') as fetch_work_item:
             fetch_work_item.return_value = [new_work_item]
@@ -74,3 +74,9 @@ class TestSyncWorkItemForJira:
                                                   new_work_item['source_display_id']):
                 assert result['display_id'] == new_work_item['source_display_id']
                 assert result['is_updated'] == True
+
+    def it_does_not_call_sync_work_item_if_its_not_defined_by_connector(self, setup_work_items, new_work_items):
+        _, work_items_sources = setup_work_items
+        empty_source = work_items_sources['empty']
+        for result in commands.sync_work_item(token_provider, empty_source.key, new_work_items[0]['source_display_id']):
+            assert result is None
