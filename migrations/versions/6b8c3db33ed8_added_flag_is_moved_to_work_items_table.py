f26d7177722988eab0ce9447e31250a7a10b8075
Pragya Goyal <pragya@64sqs.com>
2021-06-24 11:05:29 +0530

    renamed column is_moved to is_moved_from_current_source

--- migrations/versions/6b8c3db33ed8_added_flag_is_moved_to_work_items_table.py
+++ migrations/versions/6b8c3db33ed8_added_flag_is_moved_to_work_items_table.py
@@ -8,7 +8,6 @@ Create Date: 2021-06-22 10:51:55.253934
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = '6b8c3db33ed8'
 down_revision = '923f781c8511'
@@ -17,8 +16,10 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column('work_items', sa.Column('is_moved', sa.Boolean(), server_default='FALSE', nullable=True), schema='work_tracking')
+    op.add_column('work_items',
+                  sa.Column('is_moved_from_current_source', sa.Boolean(), server_default='FALSE', nullable=True),
+                  schema='work_tracking')
 
 
 def downgrade():
-    op.drop_column('work_items', 'is_moved', schema='work_tracking')
+    op.drop_column('work_items', 'is_moved_from_current_source', schema='work_tracking')
