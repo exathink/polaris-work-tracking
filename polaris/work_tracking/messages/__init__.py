2dddb83bb486f0823f33f27248bf7d8d37b3dbf8
Krishna Kumar <kkumar@exathink.com>
2023-07-16 18:43:26 -0500

    [PP-518] Refactor: rename parent_path_selectors_changed.py to work_items_source_parameters_changed.py so we can use it as the module for all parameter change messages.

--- polaris/work_tracking/messages/__init__.py
+++ polaris/work_tracking/messages/__init__.py
@@ -14,7 +14,7 @@ from .refresh_connector_projects import RefreshConnectorProjects
 from .resolve_work_items_for_epic import ResolveWorkItemsForEpic
 from .gitlab_project_event import GitlabProjectEvent
 from .trello_board_event import TrelloBoardEvent
-from .parent_path_selectors_changed import ParentPathSelectorsChanged
+from .work_items_source_parameters_changed import ParentPathSelectorsChanged
 
 
 # Add this to the global message factory so that the messages can be deserialized on receipt.
