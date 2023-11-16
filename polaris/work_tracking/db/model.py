80354be1f6484ccb20ef21902b234ac4bd6a043e
priya <pmukundan0@gmail.com>
2023-11-16 11:02:45 -0500

    [PP-661] Fixed changelog column definition

--- polaris/work_tracking/db/model.py
+++ polaris/work_tracking/db/model.py
@@ -322,7 +322,7 @@ class WorkItem(Base):
     story_points = Column(Integer, nullable=True)
     sprints = Column(ARRAY(String), nullable=True, default=[], server_default='{}')
     flagged = Column(Boolean, nullable=True, default=False, server_default='FALSE')
-    changelog = Column(JSONB, nullable=True, default={}, server_default='{}')
+    changelog = Column(JSONB, nullable=True)
 
     # timestamps for synchronization
     source_created_at = Column(DateTime, nullable=False)
